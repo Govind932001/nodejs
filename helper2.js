3defbf9a7e78c5cbeeabf9ac23c336bb27575931
Govind <mp17govind96@gmail.com>
2024-04-15 23:58:08 +0530

    15042024

---
+++ helper2.js
@@ -0,0 +1,9 @@
+function add(a,b){
+    return a+b; 
+}
+
+function add1(a,b,c){
+    return a+b+c; 
+}
+
+module.exports = {add , add1};
---
+++ index.js
@@ -0,0 +1,3 @@
+const data = require('./helper')
+
+console.log(data.add(5,2))
